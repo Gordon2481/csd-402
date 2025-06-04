27ecfb1a1dccd84e79d649974c2c616f6827b94f
MikeGordon <vprmatrix55@gmail.com>
2025-06-03 20:18:48 -0700

    finalized comments

--- module-1/WaterHeatingCalculator.java
+++ module-1/WaterHeatingCalculator.java
@@ -1,16 +1,16 @@
 /*Mike Gordon
- * 06/03/25
- * M1: Programming Assignment */
+  06/03/25
+  M1: Programming Assignment */
 
 /*This is a program that calculates the energy needed to heat water
 from an initial temperature to a final temperature. User is prompted for
 the amount of water in kilograms and the initial and final temperature of the water.*/
 
 /*Formula for use is:
- * Q = waterMass (finalTemperature - initialTemperature) * 4184
- * waterMass is water weight in kilograms
- * finalTemperature and initialTemperature are temperatures in Celsius
- * Q is the results in Joules */
+  Q = waterMass (finalTemperature - initialTemperature) * 4184
+  waterMass is water weight in kilograms
+  finalTemperature and initialTemperature are temperatures in Celsius
+  Q is the results in Joules */
 
 import java.util.*; // Allows the use of Scanner utility for user input
 
@@ -22,7 +22,8 @@ public class WaterHeatingCalculator {
         // use Scanner class to create an object to read input
         Scanner input = new Scanner(System.in);
 
-    // Step 1: get values from user
+        // Step 1: get values from user
+
         // Prompt the user to enter initial temperature
         System.out.print("What is the starting temperature in degrees Celsius?: ");
         double initialTemperature = input.nextDouble(); // Declare initialTemperature and assign value
@@ -35,10 +36,10 @@ public class WaterHeatingCalculator {
         System.out.print("What is the water weight in Kilograms?: ");
         double waterMass = input.nextDouble();   // Declare waterMass and assign value
 
-        // Step 2: calculate Q
+        // Step 2: calculate Q with user supplied values
         double Q = waterMass * (finalTemperature - initialTemperature) * 4184; // Declare and calculate Q
 
-        // Step 3: Display Q as energy needed
+        // Step 3: Display message to user showing energy needed
         System.out.println("The energy needed to heat " + waterMass + 
         " Kilograms of water from an initial temperature of " + 
         initialTemperature + "\u00B0C" + " to a final temperature of " + finalTemperature + 
