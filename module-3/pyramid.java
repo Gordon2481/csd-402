0bc0a007dcedd912c4b2375e227557fceead93b6
MikeGordon <vprmatrix55@gmail.com>
2025-06-18 20:12:35 -0700

    added code to make numbers double

--- module-3/pyramid.java
+++ module-3/pyramid.java
@@ -9,11 +9,18 @@ public class pyramid {
         int space;
 
         for (row = 1; row <= 7; ++row) {
+            // Add leading spaces to align pyramid
             for (space = 7; space > row; --space) {
                 System.out.print(" ");
             }
+
+            // Print doubling number on each row
+            // Initialize number at 1
+            int num = 1;
             for (column = 1; column <= row; column++) {
-                System.out.print(column + "");
+                System.out.print(num + " ");
+                // Double the number each iteration
+                num =  num * 2;
             }
             System.out.println();
         }
