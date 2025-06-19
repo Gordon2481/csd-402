314cbdc205d724de0744f7f4085b4462f5e0ebcd
MikeGordon <vprmatrix55@gmail.com>
2025-06-18 20:04:17 -0700

    initial logic added

--- module-3/pyramid.java
+++ module-3/pyramid.java
@@ -1,2 +1,21 @@
 // Program that uses nested for loops to output a number pyramid with @ symbols
 // on the right side
+
+public class pyramid {
+
+    public static void main(String[] args) {
+        int row;
+        int column;
+        int space;
+
+        for (row = 1; row <= 7; ++row) {
+            for (space = 7; space > row; --space) {
+                System.out.print(" ");
+            }
+            for (column = 1; column <= row; column++) {
+                System.out.print(column + "");
+            }
+            System.out.println();
+        }
+    }
+}
