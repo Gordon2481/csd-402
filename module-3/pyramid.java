e6b5724420b890dcfec68aee3922c3bd84ce3a01
MikeGordon <vprmatrix55@gmail.com>
2025-06-18 21:02:52 -0700

    basic functionality

--- module-3/pyramid.java
+++ module-3/pyramid.java
@@ -18,11 +18,41 @@ public class pyramid {
             // Initialize number at 1
             int num = 1;
             for (column = 1; column <= row; column++) {
-                System.out.print(num + " ");
+                if (num == 1 | num == 2 | num == 4 | num == 8) {
+                    System.out.print(" " + num + " ");
+                }
+                else{
+                System.out.print(num + " ");}
                 // Double the number each iteration
                 num =  num * 2;
             }
+
+            // Make the new starting number the last printed number
+            num = num / 4;
+            for (column = 1; column <= row; column++) {
+                // Handle the occurence of zero
+                if (num == 0) {
+                    System.out.print("");
+                }
+                // Print out the back half of the line
+                else if (num == 1 | num == 2 | num == 4 | num == 8) {
+                    System.out.print(" " + num + " ");
+                }
+                else{
+                System.out.print(num + " ");}
+
+                // Divide the number each iteration
+                num =  num / 2;
+            }
+
+            // Add trailing spaces with the @ at the end
+            for (space = 8; space > row; --space) {
+                System.out.print(" ");
+            }
+            System.out.print(" @");
+
             System.out.println();
+            
         }
     }
 }
