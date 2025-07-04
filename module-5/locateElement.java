b117072609241b6aa4bdf2dd6fbc6f581c400ed2
MikeGordon <vprmatrix55@gmail.com>
2025-07-03 21:12:41 -0700

    Rough outline of assignment added. Initial commit

---
+++ module-5/locateElement.java
@@ -0,0 +1,28 @@
+public class locateElement {
+/* Write methods using the following headers that returns the location of the largest element in the array 
+passed to the method returning a one-dimensional array that contains two location elements.
+public static int [ ] locateLargest (double [ ][ ] arrayParam)
+public static int [ ] locateLargest (int [ ][ ] arrayParam)*/
+
+    
+    public static int [] locateLargest(double[][] arrayParam) {
+
+    }
+
+    public static int [] locateLargest (int[][] arrayParam) {
+
+    }
+
+/* Write methods using the following headers that returns the location of the smallest element in the array 
+passed to the method returning a one-dimensional array that contains two location elements.
+public static int [ ] locateSmallest (double [ ][ ] arrayParam)
+public static int [ ] locateSmallest (int [ ][ ] arrayParam)  */
+
+    public static int [] locateSmallest(double[][] arrayParam) {
+
+    }
+
+    public static int [] locateSmallest(int[][] arrayParam) {
+        
+    }
+}
