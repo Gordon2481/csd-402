e6c6b321a7f1285edfd669eaf741be4499b5d1ae
MikeGordon <vprmatrix55@gmail.com>
2025-07-03 21:40:46 -0700

    initial code block and test usage complete

--- module-5/locateElement.java
+++ module-5/locateElement.java
@@ -6,6 +6,25 @@ public static int [ ] locateLargest (int [ ][ ] arrayParam)*/
 
     
     public static int [] locateLargest(double[][] arrayParam) {
+        // create an array to hold the location of the largest value
+        int [] location = {0,0};
+
+        // start by making the first element the max then loop through
+        double max = arrayParam[0][0];
+
+        for (int i = 0; i < arrayParam.length; i++) {
+            //loop through each column
+            for (int j = 0; j < arrayParam[i].length; j++){
+                // if a larger value is found it is made the new max
+                if (arrayParam[i][j] > max) {
+                    max = arrayParam[i][j];
+                    // save the row and column to location
+                    location[0] = i;
+                    location[1] = j;
+                }
+            }
+        }
+        return location;
 
     }
 
@@ -23,6 +42,23 @@ public static int [ ] locateSmallest (int [ ][ ] arrayParam)  */
     }
 
     public static int [] locateSmallest(int[][] arrayParam) {
-        
+
     }
+
+
+    // test usage
+    public static void main(String[] args) {
+        double[][] doubleArray = {
+            {1.1, 2.2, 3.3},
+            {4.4, 5.5, 6.6},
+            {7.7, 8.8, 9.9}
+        };
+
+        int[] doubleResult = locateLargest(doubleArray);
+        System.out.println("The largest element in the double array was found at: [" 
+        + doubleResult[0] + ", " + doubleResult[1] + "]");
+    }
+
+
+
 }
