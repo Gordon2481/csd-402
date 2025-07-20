6e271fcd314aaa7344873c254aed65d81ba735fe
MikeGordon <vprmatrix55@gmail.com>
2025-07-20 13:50:27 -0700

    created public static Integer max method

--- module-8/MikeArrayList.java
+++ module-8/MikeArrayList.java
@@ -1,16 +1,26 @@
+// import arraylist and scanner
+import java.util.ArrayList;
+import java.util.Scanner;
+
 //Create a class [yourName]ArrayListTest
 public class MikeArrayList {
+    //The method signature is to be public static Integer max (ArrayList list).
+    public static Integer max(ArrayList<Integer> list) {
+
+        // If the ArrayList is sent in empty, the method will then return 0.
+        if (list.isEmpty()) {
+            return 0;
+        }
+    }
 
     /*This class is to contain:
 A method that receives an ArrayList populated with an Integer data type holding the integers received from user input.
 The user input is to accept Integers that are then assigned to the ArrayList until a value of 0 is entered, which is also assigned to the ArrayList.
 The ArrayList is then to be sent to the method.
 The method is then to return the largest value in the ArrayList.
-If the ArrayList is sent in empty, the method will then return 0.
-The method signature is to be public static Integer max (ArrayList list).
 Write additional code for testing your method.
 The method will return the largest value that is displayed to the user. */
 
-    
+
 
 }
