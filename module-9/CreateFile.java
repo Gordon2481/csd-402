60b02fcf1c75e60aa554cf6a50ca2434d9e626b8
MikeGordon <vprmatrix55@gmail.com>
2025-07-26 13:16:01 -0700

    created file for program 2

---
+++ module-9/CreateFile.java
@@ -0,0 +1,12 @@
+public class CreateFile {
+
+    /*Write a program to create a new file titled data.file, 
+    if the file does not exist. Then write to the new file, 
+    adding 10 randomly generated numbers, or append 10 randomly 
+    generated numbers to a previous file. Each integer is to be 
+    separated by a space. Close the file, then reopen the file and 
+    read the data from the file and display it. */
+    public static void main(String[] args) {
+        
+    }
+}
