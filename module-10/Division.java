b0871de0aed890184bfa778878077deeb29770b4
MikeGordon <vprmatrix55@gmail.com>
2025-08-03 13:46:53 -0700

    module 10 files created

---
+++ module-10/Division.java
@@ -0,0 +1,11 @@
+/*Create an abstract Division class with fields for a company's division name and account number
+and an abstract display () method that will be defined in the subclasses.
+Use a constructor in the superclass that requires values for both fields.
+Create two subclasses named InternationalDivision and DomesticDivision.
+The InternationalDivision class includes a field for the country in which the division is located,
+a field for the language spoken, and a constructor that requires all fields when created.
+The DomesticDivision class includes a field for the state in which the division is located and 
+a constructor that requires all fields when created. 
+Write an application named UseDivision that creates two instances of each of these concrete classes 
+(4 total instances).
+Save the files as: Division, InternationalDivision, DomesticDivision, and UseDivision. */
