71eb713b2086d46591caedd224be2c60060aea83
MikeGordon <vprmatrix55@gmail.com>
2025-08-08 11:33:31 -0700

    added standard service

--- module-12/AutoService.java
+++ module-12/AutoService.java
@@ -9,17 +9,24 @@ Write a main method that will test each of these methods two times. */
 public class AutoService {
 
     // Constants to set pricing
+    static final double STANDARD_SERVICE = 250.00;
+    static final double OIL_CHANGE = 45.00;
+    static final double TIRE_ROTATION = 30.00;
 
     // Standard service only
+    public static double yearlyService() {
+        return STANDARD_SERVICE;
+    }
 
     // Standard service with oil change
 
     // Standard service with oil change and tire rotation
 
     // Standard sercie with added services and coupon
-}
-
-// Test each version twice
-public static void main(String[] args) {
-    
+    // Test each version twice
+    public static void main(String[] args) {
+        // Tests for no parameters
+        System.out.println("Test 1 for Standard only: $" + yearlyService());
+        System.out.println("Test 2 for Standard only: $" + yearlyService());
+    }
 }
