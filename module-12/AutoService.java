f52ebd71a0f7a45c6680d46ddf980bc7b4724dc2
MikeGordon <vprmatrix55@gmail.com>
2025-08-08 12:38:37 -0700

    finalized auto service

--- module-12/AutoService.java
+++ module-12/AutoService.java
@@ -1,10 +1,7 @@
-/*Write a program with four methods for calculating the cost of a yearly auto service visit. 
-The methods will be titled yearlyService.
-yearlyService(no parameters) - Will return the standard service charge.
-yearlyService(one parameter) - Will return the standard service charge with an added oil change fee.
-yearlyService(two parameters) - Will return the standard service charge with an added oil change fee and a tire rotation charge.
-yearlyService(three parameters) - Will return the standard service charge with an added oil change fee, a tire rotation charge, along with a coupon amount that will be deducted from the total cost.
-Write a main method that will test each of these methods two times. */
+/* Mike Gordon
+ * 8/8/25
+ * M12 Programming Assignment
+ */
 
 public class AutoService {
 
@@ -78,3 +75,8 @@ public class AutoService {
         
     }
 }
+/*
+ * Code developed based on concepts from Introduction to Java Programming and
+ * Data Structures by Y. Daniel Liang
+ * and instructional videos by Darrell Payne.
+ */
